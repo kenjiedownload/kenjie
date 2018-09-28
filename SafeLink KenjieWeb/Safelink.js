aa596e2423c779dd446d05e149f21f077ea02486
kenjiedownload <43472694+kenjiedownload@users.noreply.github.com>
2018-09-28 19:22:26 +0700

    Update Safelink.js

--- SafeLink KenjieWeb/Safelink.js
+++ SafeLink KenjieWeb/Safelink.js
@@ -25,7 +25,7 @@ if(x=="1"){xxx="/2018/09/how-ways-to-increase-your-appetite.html"}
 if(x=="2"){xxx="/2018/09/how-to-remove-eye-bags-naturally.html"}
 if(x=="3"){xxx="/2018/09/how-to-prevent-acne-on-face.html"}
 if(x=="4"){xxx="/2018/09/natural-healthy-teeth-whiten.html"}
-if(x=="5"){xxx="/2018/09/natural-ways-to-whiten-skin.html"}
+if(x=="5"){xxx="/2018/09/natural-ways-to-lose-weight.html"}
 if(x=="6"){xxx="/2018/09/natural-ways-to-whiten-skin.html"}a_to_ve[i].href="https://kenjieweb.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
